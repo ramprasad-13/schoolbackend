f3e10a0f5268e2a137366eb8d57e37c20cfb2f80
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2024-03-01 14:35:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors= require('cors');
 const hostname="0.0.0.0"
 const port = process.env.PORT || 3000;
 const app=express();
@@ -9,6 +10,15 @@ const getstudent = require('./routes/getstudent')
 const addstudent = require('./routes/addstudent')
 const delstudent = require('./routes/delstudent')
 const updatestudent = require('./routes/updatestudent')
+
+var corsOptions = {
+    origin: function (origin, callback){ callback(null, true)},
+    methods: ['GET', 'POST','PATCH'], // Specify your origin here
+    credentials: true,  // This allows the session cookie to be sent back and forth
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+
+app.use(cors(corsOptions)); // allow any origin
 
 //middlewares
 app.use(express.json())
