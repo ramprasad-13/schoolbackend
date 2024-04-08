7d751ece830e617227b83a1482336d78c9fc44c0
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2024-04-08 15:14:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const verify = require('./routes/authentication/verify');
 //middleware to authenticate
 
 var corsOptions = {
-    origin: 'https://schooldashboard-sandy.vercel.app',//function (origin, callback){ callback(null, true)},
+    origin: 'schooldashboard-sandy.vercel.app',//function (origin, callback){ callback(null, true)},
     methods: ['GET', 'POST','PATCH','DELETE'], // Specify your origin here
     credentials: true,  // This allows the session cookie to be sent back and forth
     //optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
