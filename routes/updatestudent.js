4520d7852de37dd7b62ecf3a9c7ee8c9fc3ced57
Pade venkata ramprasad <128288202+ramprasad-13@users.noreply.github.com>
2024-04-09 11:39:14 +0530

    Update updatestudent.js

--- routes/updatestudent.js
+++ routes/updatestudent.js
@@ -16,7 +16,37 @@ router.patch("/update/:id", async (req, res) => {
         if (student_found) {
             let query = { approved: false }; // Base query
             const studentsData = await students.find(query); //send unapproved students
-            res.status(200).send(studentsData);
+
+            //send mail to student who added to alumini
+            //send mail to approved student
+        const transpoter = nodemailer.createTransport({
+          service:'gmail',
+          auth:{
+            user:process.env.APP_USER,
+            pass:process.env.APP_PASSWORD
+          }
+        })
+        
+        const mailOptions = {
+          from:process.env.APP_USER,
+          to:`${current_student.email}`,
+          subject:`Great!,you are now part of saint Joseph's Alumini.`,
+          html:`
+          <h3>Hello, ${current_student.std_name}</h3>
+          <p>You have been added to Saint Joseph's Alumni. Now you are able to receive updates from us.</p>
+          <h5>Stay Connected</h5>
+          `}
+
+            //send mail here
+            transpoter.sendMail(mailOptions)
+            .then(() => {
+                return res.status(200).send(studentsData);
+            })
+            .catch((error) => {
+                console.error(error);
+                return res.status(500).json({ message: "Failed to send mail to User [ verified sucessfully ]!" });
+            });
+        
         } else {
             res.status(404).send("Student not found!");
         }
